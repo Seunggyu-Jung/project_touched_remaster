a4dbb2a5222e801e1afa25387188f798a58c4ba1
Daniel <wswm2007@naver.com>
2023-10-23 16:36:40 +0900

    chore: recoil 초기 셋팅 설정 및 infopage 상태관리 설정

---
+++ src/Pages/InfoPage/InfoPage.styled.ts
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components';
+
+export const InfoDiv = styled.div`
+  width: auto;
+  margin-top: 80px;
+  text-align: center;
+`;
--- src/Pages/InfoPage/InfoPage.tsx
+++ src/Pages/InfoPage/InfoPage.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { userState, nameState } from './recoil';
+import * as S from './InfoPage.styled';
 
 export default function InfoPage() {
+  const [user, setUser] = useRecoilState(userState);
+  const [name, setName] = useRecoilState(nameState);
+
   return (
-    <div>
+    <S.InfoDiv>
       <h1>편지에 작성할 기본 정보들을 입력하는 단계입니다.</h1>
-      <p>당신의 성함은?</p>
-      <p>이 편지를 받아 감동할 분의 성함은?</p>
-    </div>
+      <form>
+        <p>당신의 성함은?</p>
+        <input
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <p>이 편지를 받고 감동할 분의 성함은?</p>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      <Link to="/">
+        <button>이전</button>
+      </Link>
+      <Link to="/write">
+        <button>다음</button>
+      </Link>
+    </S.InfoDiv>
   );
 }
