03e5a651b65ac48dde7048a111fc89ddaf51a6cf
Daniel <wswm2007@naver.com>
2023-10-29 23:16:58 +0900

    feat: ImagePage 추가

---
+++ src/Pages/ImagePage/ImagePage.styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ImageDiv = styled.div`
+  width: auto;
+  margin-top: 80px;
+  text-align: center;
+`;
+
+export const DeleteBtn = styled.button`
+  position: absolute;
+  top: 0;
+  right: 25px;
+`;
+
+export const ImageInput = styled.input`
+  margin-bottom: 10px;
+`;
+
+export const ImageSection = styled.section`
+  max-width: 400px;
+  display: block;
+  margin: auto;
+`;
