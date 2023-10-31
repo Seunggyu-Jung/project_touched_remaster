c505506385178f3d4c1a4cf542797dc19557d3e9
Daniel <wswm2007@naver.com>
2023-10-31 23:31:48 +0900

    feat: 페이지 순서 재배치

--- src/Pages/FirstPage/FirstPage.tsx
+++ src/Pages/FirstPage/FirstPage.tsx
@@ -7,7 +7,7 @@ export default function FirstPage() {
     <S.mainDiv>
       <h1>스타트 페이지</h1>
       <p>감사한 분에게 여러분만의 편지를 만들어 마음을 전달해봐요</p>
-      <Link to="/info">
+      <Link to="/background">
         <button>편지 작성하러 이동</button>
       </Link>
     </S.mainDiv>
