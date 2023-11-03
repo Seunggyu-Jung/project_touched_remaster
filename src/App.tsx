3df0bd60f697b869c44aad82a256e98af5a344e0
Daniel <wswm2007@naver.com>
2023-11-03 23:06:14 +0900

    feat: GlobalStyle body값 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import WrittingPage from './Pages/WrittingPage/WrittingPage';
 import TestPage from './Pages/TestPage/TestPage';
 import ImagePage from './Pages/ImagePage/ImagePage';
 import BackgroundPage from './Pages/BackgroundPage/BackgroundPage';
+import CheckPage from './Pages/CheckPage/CheckPage';
 import { useRecoilValue } from 'recoil';
 import { backgroundState } from './Pages/recoil';
 import { createGlobalStyle } from 'styled-components'
@@ -16,16 +17,14 @@ function App() {
   const background = useRecoilValue(backgroundState)
 
   const GlobalStyles = createGlobalStyle`
-  body {
-    height: 900px;
+    body, html {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
     background: linear-gradient(to bottom, ${background[0]} , ${background[1]});
-
-    @media (max-width: 400px) {
-    body {
-      height: 500px;
-    }
   }
-  }`
+  `
 
 
   return (
@@ -39,6 +38,7 @@ function App() {
           <Route path="/test" element={<TestPage />}></Route>
           <Route path="/image" element={<ImagePage />}></Route>
           <Route path="/background" element={<BackgroundPage />}></Route>
+          <Route path="/check" element={<CheckPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
