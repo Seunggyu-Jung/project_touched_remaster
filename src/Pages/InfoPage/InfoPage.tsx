ed9a94bd8cf241b535150ff0d4bf54d310ce9340
DanielQ <wswm2007@naver.com>
2023-11-22 22:23:20 +0900

    feat: 정보 페이지에 guideSection 추가

--- src/Pages/InfoPage/InfoPage.tsx
+++ src/Pages/InfoPage/InfoPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { userState, nameState } from '../recoil';
 import Button from '../../Common/Button/Button'
+import Guide from '../../Common/GuideSection/GuideSection';
 import * as S from './InfoPage.styled';
 
 export default function InfoPage() {
@@ -23,18 +24,23 @@ export default function InfoPage() {
   return (
     <S.InfoDiv>
       <h1>편지에 작성할 기본 정보들을 입력하는 단계입니다.</h1>
+      <Guide title='발신자와 수신자 설정 방법' step1='1. 상단에 있는 칸에는 발신자 분의 성함을 입력해주세요.' step2='2. 수신 하단에 있는 칸에는 수신자 분의 성함을 입력해주세요.'
+        step3='*해당 정보는 필수 정보이기에 꼭 입력해주세요!!' ></Guide>
+
       <form>
         <p>당신의 성함은?</p>
         <input
           type="text"
           value={user}
           onChange={(e) => setUser(e.target.value)}
+          placeholder='발신자 성함'
         />
         <p>이 편지를 받고 감동할 분의 성함은?</p>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
+          placeholder='수신자 성함'
         />
       </form>
 
