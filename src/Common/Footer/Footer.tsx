311f34ce784711ab2b0ed533f6032a3cb5bd429d
DanielQ <wswm2007@naver.com>
2023-11-30 23:51:40 +0900

    feat: 공용 컴퍼넌트에 헤더와 풋터 컴퍼넌트 추가

---
+++ src/Common/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './Footer.styled'
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div>풋터입니다.</div>
+      </footer>
+    </>
+  )
+}
--- src/Pages/FirstPage/FirstPage.tsx
+++ src/Pages/FirstPage/FirstPage.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as S from './FirstPage.styled';
+import Header from '../../Common/Header/Header';
+import Footer from '../../Common/Footer/Footer';
 
 export default function FirstPage() {
   return (
     <S.mainDiv>
+      <Header />
       <h1>스타트 페이지</h1>
       <p>감사한 분에게 여러분만의 편지를 만들어 마음을 전달해봐요</p>
       <Link to="/background">
         <button>편지 작성하러 이동</button>
       </Link>
+      <Footer />
     </S.mainDiv>
   );
 }
