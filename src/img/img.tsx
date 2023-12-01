52c682f20edf3bb7929ed8b9b8065685cb4d4825
DanielQ <wswm2007@naver.com>
2023-12-01 17:27:14 +0900

    feat: 이미지 파일 변경

--- src/img/img.tsx
+++ src/img/img.tsx
@@ -4,6 +4,6 @@ export { default as img3 } from './편지지3.avif';
 export { default as img4 } from './편지지4.jpg';
 export { default as img5 } from './편지지5.avif';
 export { default as guide1 } from './Frame 1.svg';
-export { default as guide2 } from './Frame4.svg';
+export { default as guide2 } from './Frame J4.png';
 export { default as guide3 } from './Frame 5.svg';
 export { default as back } from './guideBack.avif'
