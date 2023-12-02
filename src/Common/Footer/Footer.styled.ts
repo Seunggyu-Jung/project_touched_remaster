f71070bb6d0e4bc32b97a7a108c2c53ff3e503c6
DanielQ <wswm2007@naver.com>
2023-12-02 23:15:23 +0900

    feat: footer 항시 하단 고정 기능 추가

--- src/Common/Footer/Footer.styled.ts
+++ src/Common/Footer/Footer.styled.ts
@@ -1,1 +1,8 @@
 import styled from 'styled-components';
+
+export const footer = styled.footer`
+  border: 1px solid black;
+  height: 100px;
+  position: relative;
+  transform: translateY(-100%);
+`;
--- src/Common/Footer/Footer.tsx
+++ src/Common/Footer/Footer.tsx
@@ -4,9 +4,7 @@ import * as S from './Footer.styled'
 export default function Footer() {
   return (
     <>
-      <footer>
-        <div>풋터입니다.</div>
-      </footer>
+      <S.footer></S.footer>
     </>
   )
 }
