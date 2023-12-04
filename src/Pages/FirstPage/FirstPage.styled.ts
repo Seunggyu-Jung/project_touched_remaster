9df3332bb3031fd6161a90f0dbde6d8489a3480a
DanielQ <106158869+Seunggyu-Jung@users.noreply.github.com>
2023-12-04 23:55:18 +0900

    Revert "feat: 시작 페이지 UI 및 구성 변경"

--- src/Pages/FirstPage/FirstPage.styled.ts
+++ src/Pages/FirstPage/FirstPage.styled.ts
@@ -2,32 +2,6 @@ import { styled } from 'styled-components';
 
 export const mainDiv = styled.div`
   width: auto;
-  height: auto;
-  min-height: calc(100% - 200px);
-  padding-bottom: 200px;
+  margin-top: 80px;
   text-align: center;
-  background-color: #ffff;
-`;
-
-export const mainSection = styled.section``;
-
-export const mainButton = styled.button`
-  width: 100px;
-  height: 100px;
-  border: 0.5px solid #f2f2f2;
-  border-radius: 10px;
-  margin-top: 20px;
-
-  :hover {
-    opacity: 0.6;
-    & {
-      transform: rotate(1turn);
-      transition: transform 1s;
-    }
-  }
-`;
-
-export const buttonImage = styled.img`
-  width: 100%;
-  height: 100%;
 `;
